d5401412d17d586fa6004cba881e451182875f9f
shun91 <shun9167@gmail.com>
2023-05-21 15:48:55 +0900

    accessTokenが不正なら401を返す

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from "@google-cloud/functions-framework";
 import { Client } from "@notionhq/client";
+import { IncomingHttpHeaders } from "node:http";
 
 const accessToken = process.env.ACCESS_TOKEN;
 const apiKey = process.env.NOTION_API_KEY;
@@ -7,10 +8,20 @@ const databaseId = "50c2c7504a404f30bc7a222e8b565396";
 
 const notion = new Client({ auth: apiKey });
 
+const authorize = (headers: IncomingHttpHeaders) => {
+  return headers.authorization === `Bearer ${accessToken}`;
+};
+
 export const helloHttp = functions.http(
   "helloHttp",
   async (req: functions.Request, res: functions.Response) => {
-    console.log("★authorization:", req.headers.authorization);
+    // accessTokenが不正なら401を返す
+    if (req.headers.authorization !== `Bearer ${accessToken}`) {
+      const error = { status: 401, message: "Unauthorized" };
+      console.error(error);
+      res.status(401).json(error);
+      return;
+    }
 
     const url = "https://twitter.com/kojo_73/status/3874734738";
     const text = "for gcf";
